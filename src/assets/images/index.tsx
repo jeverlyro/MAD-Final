b4d13c92b087f05ca04567ef825687b9df807511
Thimothy25 <thimothypogantung27@gamil.com>
2024-11-10 13:19:28 +0800

    add asset swc

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,5 +1,8 @@
 import akko from './akko.png';
 import kaychron from './qmk.png';
 import gamakay from './168.png';
+import akkoswc from './akko.swtc.png';
+import geteswc from './gete.swcth.png';
+import hmxswc from './Hmx.swcth.png';
 
-export {akko, kaychron, gamakay};
+export {akko, kaychron, gamakay, akkoswc, geteswc, hmxswc};
