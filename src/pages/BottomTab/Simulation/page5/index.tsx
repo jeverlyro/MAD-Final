b09825363f76b58b0c375e730f7b07c6a1d35d72
Thimothy25 <thimothypogantung27@gamil.com>
2024-11-10 13:34:36 +0800

    Adding Page 4 sim

--- src/pages/BottomTab/Simulation/page5/index.tsx
+++ src/pages/BottomTab/Simulation/page5/index.tsx
@@ -3,36 +3,35 @@ import {
   View,
   Text,
   StyleSheet,
-  TouchableOpacity,
   ScrollView,
+  TouchableOpacity,
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useNavigation} from '@react-navigation/native';
-import Card from '../../../../molecules/card';
+import {akkoswc, geteswc, hmxswc} from '../../../../assets/images';
+import CardB from '../../../../molecules/CardB';
 
-const Plans: React.FC = () => {
+const SelectionSwicth = () => {
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Make Your Plans</Text>
-      </View>
-
+      <Text style={styles.headerTitle}>Select Your Switches</Text>
       <View style={styles.divider} />
 
       <ScrollView contentContainerStyle={styles.scrollContainer}>
-        {[
-          'Choose your barebone kit',
-          'Choose your switches',
-          'Choose your keycaps',
-          'Additional',
-        ].map((item, index) => (
-          <Card key={index} title={item} />
-        ))}
-
-        <TouchableOpacity style={styles.saveButton}>
-          <Text style={styles.saveButtonText}>Save plan</Text>
+        <TouchableOpacity>
+          <CardB
+            title="Akko Creamy Yellow
+V3 Pro"
+            imageSource={akkoswc}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <CardB title="Gateron Oil King" imageSource={geteswc} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <CardB title="HMX Xinhai" imageSource={hmxswc} />
         </TouchableOpacity>
       </ScrollView>
 
@@ -66,46 +65,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#121927',
     paddingTop: 20,
   },
-  header: {
-    alignItems: 'center',
-    marginBottom: 10,
-    marginTop: 10,
-  },
   headerTitle: {
     fontSize: 26,
     fontFamily: 'DM-Sans',
     color: 'white',
     fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
   },
   divider: {
     width: '90%',
     height: 1,
     backgroundColor: '#5046E5',
     opacity: 0.5,
+    marginBottom: 10,
     alignSelf: 'center',
-    marginVertical: 10,
   },
   scrollContainer: {
     paddingHorizontal: 20,
-    marginTop: 10,
-  },
-  saveButton: {
-    backgroundColor: '#5046E5',
-    paddingVertical: 10,
-    alignItems: 'center',
-    borderRadius: 20,
-  },
-  saveButtonText: {
-    color: 'white',
-    fontSize: 14,
-    fontWeight: 'bold',
   },
   tabBar: {
     position: 'absolute',
     bottom: 0,
     height: 80,
     width: '100%',
-    backgroundColor: 'transparent',
+    backgroundColor: 'transparent ',
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
@@ -114,6 +98,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
   },
+  tabText: {
+    fontFamily: 'Inter-Regular',
+    color: 'white',
+    fontSize: 10,
+    marginTop: 4,
+  },
 });
 
-export default Plans;
+export default SelectionSwicth;
